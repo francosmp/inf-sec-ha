1421d88f47394b54ebff76c2887f5d76e025a432
Franco Mp <stha-182@hotmail.com>
2019-10-15 04:00:09 -0500

    2019/10/15 04:00 nth

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,51 +1,43 @@
 import React from 'react';
-
 import '../css/tablas.css';
 import '../css/botones.css';
 import '../css/extra.css';
 import '../css/input.css';
-
 import axios from 'axios';
-
 class Body extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
+      root: 'https://b83b39cd.ngrok.io/inf-sec-php-ser/servicios-php.php',
+      mirror: 'http://urlMirror/inf-sec-php-ser/servicios-php.php',
       data: [],
       index: '',
       codigo: ''
     };
-
     this.postCreate = this.postCreate.bind(this);
     this.postRead = this.postRead.bind(this);
     this.postUpdate = this.postUpdate.bind(this);
     this.postDelete = this.postDelete.bind(this);
-
     this.extra = this.extra.bind(this);
-
     this.handleIndex = this.handleIndex.bind(this);
     this.handleCodigo = this.handleCodigo.bind(this);
   }
-
   componentDidMount() {
     console.log("componentDidMount");
     this.postRead();
   }
-
   postCreate() {
     if (this.state.codigo === '') {
       alert("Codigo Vacio");
     } else {
-      axios.post('http://localhost/inf-sec-php-ser/servicios-php.php', { "crud": "create", "index": "0", "codigo": this.state.codigo })
+      axios.post(this.state.root, { "crud": "create", "index": "0", "codigo": this.state.codigo })
         .then(res => {
           this.setState({ index: '', codigo: '' }, function () { this.postRead(); }.bind(this));
         })
     }
   }
-
   postRead() {
-    axios.post('http://localhost/inf-sec-php-ser/servicios-php.php', { "crud": "read", "index": "0", "codigo": "0" })
+    axios.post(this.state.root, { "crud": "read", "index": "0", "codigo": "0" })
       .then(res => {
         this.setState({ data: Object.values(res.data) }, function () {
           //console.log(res);
@@ -53,37 +45,30 @@ class Body extends React.Component {
         }.bind(this));
       })
   }
-
   postUpdate() {
     if (this.state.codigo === '' || this.state.index === '') {
       alert("Codigo o Index Vacio");
     } else {
-      axios.post('http://localhost/inf-sec-php-ser/servicios-php.php', { "crud": "update", "index": this.state.index, "codigo": this.state.codigo })
+      axios.post(this.state.root, { "crud": "update", "index": this.state.index, "codigo": this.state.codigo })
         .then(res => {
           this.setState({ index: '', codigo: '' }, function () { this.postRead(); }.bind(this));
         })
     }
   }
-
   postDelete() {
-    axios.post('http://localhost/inf-sec-php-ser/servicios-php.php', { "crud": "delete", "index": "0", "codigo": "0" })
+    axios.post(this.state.root, { "crud": "delete", "index": "0", "codigo": "0" })
       .then(res => {
         this.postRead();
       });
   }
-
   extra() {
-
   }
-
   handleIndex(e) {
     this.setState({ index: e.target.value.replace(/\D+/, '') }, function () { console.log(this.state.index); }.bind(this));
   }
-
   handleCodigo(e) {
     this.setState({ codigo: e.target.value }, function () { console.log(this.state.codigo); }.bind(this));
   }
-
   render() {
     return (
       <div className="App-body">
@@ -105,14 +90,12 @@ class Body extends React.Component {
           </table>
         </div>
         <div className="extra">
-
         </div>
         <div>
           <input className="inputB" type="text" placeholder="Index" value={this.state.index} onChange={this.handleIndex.bind(this)} />
           <input className="inputB" type="text" placeholder="Codigo" value={this.state.codigo} onChange={this.handleCodigo.bind(this)} />
         </div>
         <div className="extra">
-
         </div>
         <div>
           <button className="button button1" onClick={this.postCreate}>Create</button>
@@ -124,5 +107,4 @@ class Body extends React.Component {
     );
   }
 }
-
 export default Body;
